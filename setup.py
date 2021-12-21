d5965bd71aefff53051d1fca6cdcadc16fd0a784
bmxitalia <tommasocarraro96@gmail.com>
2021-12-21 17:20:15 +0100

    uploaded setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='LTNtorch',
+    version='0.9',
+    packages=find_packages(where='ltn'),
+    install_requires=[
+        "numpy",
+        "torch"
+    ],
+    python_requires='>=3.7',
+    url='https://github.com/bmxitalia/LTNtorch',
+    download_url='https://github.com/bmxitalia/LTNtorch',
+    license='MIT',
+    author='Tommaso Carraro',
+    author_email='tommaso.carraro@studenti.unipd.it',
+    description='LTNtorch: PyTorch implementation of Logic Tensor Networks',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['pytorch', 'machine-learning', 'framework', 'neural-symbolic-computing', 'fuzzy-logic'],
+    classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Programming Language :: Python :: 3.9',
+            'Topic :: Scientific/Engineering :: Artificial Intelligence',
+            'License :: OSI Approved :: MIT License',
+        ]
+)
