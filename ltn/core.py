fixed issue on process_ltn_objects() not performing deep copy on variables with gradient

ltn/core.py
@@ -266,11 +266,11 @@ def process_ltn_objects(objects):
     # we want to give the user the possibility to use the same object in different formulas
     # if we do not perform a deep copy, the object itself will be changed by this function even outside of the function
     # due to a side effect
-    # note that we copy only if the input object is a constant with grad_fn or if the object has not grad_fn attribute,
-    # namely it is a leaf tensor
+    # note that we copy only if the input object is a constant/variable with grad_fn or if the object has not
+    # grad_fn attribute, namely it is a leaf tensor
     objects_ = [LTNObject(torch.clone(o.value), copy.deepcopy(o.free_vars))
-                if (o.value.grad_fn is None or (isinstance(o, Constant) and o.value.grad_fn is not None)) else o
-                for o in objects]
+                if (o.value.grad_fn is None or (isinstance(o, (Constant, Variable)) and o.value.grad_fn is not None))
+                else o for o in objects]
     # this deep copy is necessary to avoid the function directly changes the free
     # variables and values contained in the given LTN objects. We do not want to directly change the input objects
     # Instead, we need to create new objects based on the input objects since it is possible we have to reuse the

tests/tests.py
@@ -164,7 +164,7 @@ def test_process_ltn_objects():
     assert proc_objs[0].free_vars == proc_objs[1].free_vars == ['x'], "The two LTN objects should now share " \
                                                                       "the same variable."
 
-    # same test but with a constant that is now trainable -> no deep copy should be performed by the function
+    # same test but with a constant that is now trainable -> deep copy should be performed by the function
     c1_t = Constant(torch.tensor([1, 2, 3, 4]), trainable=True)
     # put a gradient into c1_t to test a functionality
     c1_t.value = torch.unsqueeze(c1_t.value, 1)
@@ -185,7 +185,7 @@ def test_process_ltn_objects():
     proc_objs, _, _ = process_ltn_objects([c1_t, v1_])
 
     assert proc_objs[0] is not c1_t, "A deep copy should be performed, since c1 is a constant with grad_fn."
-    assert proc_objs[1] is v1_, "A deep copy should not be performed since v1_ has grad_fn."
+    assert proc_objs[1] is not v1_, "A deep copy should be performed, since v1_ is a variable with grad_fn."
 
     # test of the function with two variables in input
 