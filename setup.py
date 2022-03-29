f8cbef243ed99f44d4e8184cd85de883b69c7f86
bmxitalia <tommasocarraro96@gmail.com>
2022-03-29 15:24:23 +0200

    change settings for new pypi package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='LTNtorch',
-    version='0.9',
+    version='1.0.0',
     packages=find_packages(include=['ltn']),
     install_requires=[
         "numpy",
