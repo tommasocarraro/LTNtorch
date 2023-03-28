8c8bc9be323622d5cf4273c5aeef4caeaba1d873
bmxitalia <tommasocarraro96@gmail.com>
2023-03-28 14:39:30 +0200

    new PyPi package has been created

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='LTNtorch',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(include=['ltn']),
     install_requires=[
         "numpy",
